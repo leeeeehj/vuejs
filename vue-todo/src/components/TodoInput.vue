7282622bc3c88c4e22f8b3bad504685af4447473
hyunju <hyunju920424@gmail.com>
2024-02-27 10:28:43 +0900

    second commit

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -4,27 +4,48 @@
     <span class="addContainer" @click="addTodo">
         <i class="fa fa-plus-circle addBtn" aria-hidden="true"></i>
     </span>
+    <Modal v-if="showModal" @close="showModal = false">
+        <template #header>
+            <h3>warning</h3>
+        </template>
+        <template #body>
+            nothing typed to add
+        </template>
+        <template #footer>
+            <span class="closeBtn" @click="showModal = false">
+                <i class="fa-solid fa-circle-check addBtn"></i>
+             </span>
+        </template>
+    </Modal>
   </div>
 </template>
 
 <script>
+import Modal from './common/Modal.vue'
 export default {
     data: function(){
         return{
-            newTodoItem: ""
+            newTodoItem: "",
+            showModal: false
         }
     },
     methods:{
         //모든 기능을 app.vue 로 위임하고 emit만 사용하여 처리
         addTodo: function(){
             if (this.newTodoItem !== ''){
-                this.$emit('addTodoItem',this.newTodoItem)
+                this.$emit('addTodoItem',this.newTodoItem);
                 this.clearInput();
+                }else{
+                    this.showModal = !this.showModal;
                 }
         },
         clearInput: function(){
             this.newTodoItem = '';
         }
+    },
+    components: {
+       Modal : Modal
+
     }
 }
 </script>
@@ -52,7 +73,13 @@ input:focus{
     width: 3rem;
     border-radius: 0 5px 5px 0;
 }
-
+.closeBtn{
+    float: right;
+    background: linear-gradient(to right, #6478F8, #8763FB);
+    display: block;
+    width: 3rem;
+    border-radius: 5px 5px 5px 5px;
+}
 .addBtn{
     color: white;
     vertical-align: middle;
